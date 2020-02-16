b10aa031b24f214d7f3c56ae60199352b3c7344b
Dell <diwashlimbu47@gmail.com>
2020-02-16 18:50:30 +0545

    Prevalent

--- app/src/main/java/com/example/farmersagriculture/Prevalent/Prevalent.java
+++ app/src/main/java/com/example/farmersagriculture/Prevalent/Prevalent.java
@@ -6,7 +6,7 @@ import com.example.farmersagriculture.model.Users;
 public class Prevalent {
     public   static Users currentUsers;
 
-    public  static Admin currentAdmin;
+
 
 
     public static final String UserPhoneKey="UserPhone";
